b37f69457bc809341adccc4666ac7341af9ad429
sarvscode <psmk156@gmail.com>
2025-09-19 16:16:32 +0530

    second file

---
+++ src/main/java/com/example/MovieTicketBookRealProject/MovieTicketBookRealProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieTicketBookRealProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookRealProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookRealProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MovieTicketBookRealProject/MovieTicketBookRealProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieTicketBookRealProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookRealProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
