377a34fb323b6c1c72b0e75aff8e59ac4de64be1
sarvscode <psmk156@gmail.com>
2025-09-20 22:47:11 +0530

    second time commit

--- src/main/java/com/example/MovieTicketBookRealProject/Controller/BookingController.java
+++ src/main/java/com/example/MovieTicketBookRealProject/Controller/BookingController.java
@@ -1,5 +1,25 @@
 package com.example.MovieTicketBookRealProject.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.MovieTicketBookRealProject.DTO.BookingDTO;
+import com.example.MovieTicketBookRealProject.Entity.Booking;
+import com.example.MovieTicketBookRealProject.Service.BookingService;
+
+@RestController
+@RequestMapping("/api/booking")
 public class BookingController {
 
+	 
 }
